cd0b3f397e92030adc147bc475985e63beb9ccf8
Pedro Rocha <pedrojrocha@outlook.com>
2023-11-04 11:01:43 +0100

    First commit with OpenAI config wizard

---
+++ config/notebook_config.py
@@ -0,0 +1,91 @@
+#
+# Config class to store all the configuration variables
+# Requests the user for the following information:
+#   - Azure or OpenAI
+#   - Azure Endpoint
+#   - Azure Model
+#   - Azure API Key
+#
+# This class will be used in Visual Studio Code, requesting input from the user from a Jupyter Notebook.
+#
+import json
+
+# Define default values
+DEFAULT_API_TYPE = "azure"
+DEFAULT_MODEL = "gpt-35-turbo"
+DEFAULT_MODEL_VERSION = "2023-05-15"
+
+class notebook_config:
+    def __init__(self):
+        self.config_file = "../config/settings.json"
+        self.api_type = DEFAULT_API_TYPE
+        self.endpoint = ""
+        self.model = DEFAULT_MODEL
+        self.model_version = DEFAULT_MODEL_VERSION
+        self.azure_api_key = ""
+
+    def ask_api_type(self):
+        api_type = input("Which API would you like to use? (Azure or OpenAI): ").lower()
+        if api_type == "azure":
+            self.api_type = "azure"
+        elif api_type == "openai":
+            self.api_type = "openai"
+        else:
+            raise ValueError(f"Invalid API type {api_type}. Please enter either Azure or OpenAI.")
+
+        self.save_config_to_file()
+
+    def ask_endpoint(self):
+        if self.api_type == "azure":
+            endpoint = input("Please enter your endpoint: ")
+            if endpoint == "":
+                raise ValueError("Endpoint cannot be empty.")
+            self.endpoint = endpoint
+            self.save_config_to_file()
+
+    def ask_model(self):
+        if self.api_type == "azure":
+            model = input(f"Please enter your model (press ENTER for {DEFAULT_MODEL}): ")
+            if model == "":
+                model = DEFAULT_MODEL
+            self.model = model
+            self.save_config_to_file()
+
+    def ask_api_key(self):
+        azure_api_key = input("Please enter your API key: ")
+        if azure_api_key == "":
+            raise ValueError("API key cannot be empty.")
+        self.azure_api_key = azure_api_key
+        self.save_config_to_file()
+
+    def ask_model_version(self):
+        if self.api_type == "azure":
+            model_version = input(f"Please enter your model version (press ENTER for {DEFAULT_MODEL_VERSION}): ")
+            if model_version == "":
+                model_version = DEFAULT_MODEL_VERSION
+            self.model_version = model_version
+            self.save_config_to_file()
+
+    def save_config_to_file(self):
+        with open(self.config_file, "w") as f:
+            json.dump(self.__dict__, f)
+
+    def load_config_from_file(self):
+        with open(self.config_file, "r") as f:
+            self.__dict__ = json.load(f)
+            # Load config to environment variables
+            self.load_config_to_env()
+
+
+    def load_config_to_env(self):
+        import os
+
+        if self.api_type == "azure":
+            os.environ["OPENAI_API_TYPE"] = self.api_type
+            os.environ["AZURE_OPENAI_KEY"] = self.azure_api_key
+            os.environ["AZURE_OPENAI_ENDPOINT"] = self.endpoint
+            os.environ["AZURE_OPENAI_MODEL"] = self.model
+            os.environ["AZURE_OPENAI_MODEL_VERSION"] = self.model_version
+        elif self.api_type == "openai":
+            os.environ["OPENAI_API_TYPE"] = self.api_type
+            os.environ["OPENAI_KEY"] = self.azure_api_key
