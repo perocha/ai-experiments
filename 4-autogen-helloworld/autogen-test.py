3d35a201a68820246568feec084855c8ca3b9e61
Pedro Rocha <pedrojrocha@outlook.com>
2023-11-06 18:35:02 +0100

    Refactor Python autogen test

--- 4-autogen-helloworld/autogen-test.py
+++ 4-autogen-helloworld/autogen-test.py
@@ -1,24 +1,30 @@
+# Import the autogen module
 from autogen import AssistantAgent, UserProxyAgent, get_config_list
-import os
 
+# Instatiate the config class
+import sys
+sys.path.append("../config")
+from notebook_config import notebook_config
+config = notebook_config()
+# Load config from file
+config.load_config_from_file()
 
-api_type = "azure"
-endpoint = "https://perocha.openai.azure.com"
-model = "gpt-35-turbo"
-model_version = "2023-05-15"
-azure_api_key = "f712a34b52a740d0a6f8fe36fc7ed198"
-
+# Get the config
+api_type = config.get_api_type()
+endpoint = config.get_endpoint()
+model = "gpt-35-turbo" # It's hardcoded in autogen to gpt4
+model_version = config.get_model_version()
+azure_api_key = config.get_api_key()
 config_list = get_config_list(
     api_keys=[azure_api_key],
     api_bases=[endpoint],
     api_type=api_type,
     api_version=model_version
 )
-
 print(config_list)
-
 
 assistant = AssistantAgent(name="Assistant", llm_config=config_list)
 
+input_str = input("How can I help you? ")
 user_proxy = UserProxyAgent(name="user_proxy", code_execution_config={"work_dir": "coding"}, llm_config=config_list)
-user_proxy.initiate_chat(assistant, message="Plot a chart of NVDA and TESLA stock price change YTD.")
+user_proxy.initiate_chat(assistant, message=input_str)
