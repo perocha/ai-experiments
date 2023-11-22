342fc7b84b9e16cd671af3289d2a232a07c38f33
Pedro Rocha <pedrojrocha@outlook.com>
2023-11-22 22:36:29 +0100

    Azure Form regognizer SK function

---
+++ plugins/form_recognizer/form_recognizer_connector.py
@@ -0,0 +1,51 @@
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.formrecognizer import DocumentAnalysisClient, AnalyzeResult
+from typing import Optional
+from logging import Logger
+from semantic_kernel.utils.null_logger import NullLogger
+
+class FormRecognizerConnector():
+
+    _api_key: str
+
+    def __init__(self, endpoint: str, api_key: str, logger: Optional[Logger] = None) -> None:
+        self._api_key = api_key
+        self._endpoint = endpoint
+        self._logger = logger if logger else NullLogger()
+
+        if not self._endpoint:
+            raise ValueError("FormRecognizer endpoint cannot be null.")
+
+        if not self._api_key:
+            raise ValueError("FormRecognizer API key cannot be null.")
+
+    def recognize_invoice_async(self, filename_path: str) -> str:
+
+        # Create an instance of a Form Recognizer client
+        document_analysis_client = DocumentAnalysisClient(
+            endpoint=self._endpoint, credential=AzureKeyCredential(self._api_key)
+        )
+
+        poller = document_analysis_client.begin_analyze_document("prebuilt-invoice", open(filename_path, "rb"))
+
+        invoices = poller.result()
+
+        invoice = invoices.documents[0]
+        invoice_total = invoice.fields.get("InvoiceTotal")
+        customer_name = invoice.fields.get("CustomerName")
+        invoice_id = invoice.fields.get("InvoiceId")
+        invoice_date = invoice.fields.get("InvoiceDate")
+        invoice_total_value = str(invoice_total.value) if invoice_total and invoice_total.value else None
+        customer_name_value = str(customer_name.value) if customer_name and customer_name.value else None
+        invoice_id_value = str(invoice_id.value) if invoice_id and invoice_id.value else None
+        invoice_date_value = str(invoice_date.value) if invoice_date and invoice_date.value else None
+
+        self._logger.info("Invoice Total: {}".format(invoice_total_value))
+        self._logger.info("Customer Name: {}".format(customer_name_value))
+        self._logger.info("Invoice ID: {}".format(invoice_id_value))
+        self._logger.info("Invoice Date: {}".format(invoice_date_value))
+
+        # Create a string with the results, separated by ";"
+        result = ";".join([invoice_total_value, customer_name_value, invoice_id_value, invoice_date_value])
+
+        return result
---
+++ plugins/form_recognizer/sk_form_recognizer.py
@@ -0,0 +1,46 @@
+from semantic_kernel.skill_definition import sk_function
+from plugins.form_recognizer.form_recognizer_connector import FormRecognizerConnector
+import logging
+
+class FormRecognizer:
+    """
+    A plugin to read an Invoice.
+    """
+
+    def __init__(self, endpoint: str, api_key: str):
+        if not endpoint:
+            raise Exception("FormRecognizer endpoint is not set")
+        if not api_key:
+            raise Exception("FormRecognizer API key is not set")
+
+        # Create a logger
+        logger = logging.getLogger('my_logger')
+        logger.setLevel(logging.INFO)
+
+        # Create a file handler
+        handler = logging.FileHandler('my_log.log')
+        handler.setLevel(logging.INFO)
+
+        # Add the handler to the logger
+        logger.addHandler(handler)
+
+        # Create an instance of the FormRecognizerConnector
+        self.form_recognizer = FormRecognizerConnector(endpoint=endpoint, api_key=api_key, logger=logger)
+
+    @sk_function(
+        description="Use Form Recognizer to process an invoice. The result is a dictionary of the detected fields.",
+        name="process_invoice",
+        input_description="The path to the invoice file.",
+    )
+    def process_invoice(self, filename_path: str) -> str:
+        """
+        A native function that uses Form Recognizer to process an invoice.
+        """
+
+        # Input validation, the error message can help self-correct the input
+        if not filename_path:
+            raise ValueError("The filename_path argument cannot be null.")
+
+        result = self.form_recognizer.recognize_invoice_async(filename_path=filename_path)
+
+        return result
